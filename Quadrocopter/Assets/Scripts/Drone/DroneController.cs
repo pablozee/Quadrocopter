1f0feffc9967c352b277e1ccf728e8a3c0228654
Zeshan Rasul <zeshanrasul@gmail.com>
2022-02-25 20:37:35 +0000

    Drone pitch, roll and yaw hooked up to controls

--- Quadrocopter/Assets/Scripts/Drone/DroneController.cs
+++ Quadrocopter/Assets/Scripts/Drone/DroneController.cs
@@ -8,11 +8,16 @@ public class DroneController : BaseRigidbody
 {
     [Header("Drone Properties")]
     [SerializeField] private float minMaxPitch = 30f;
-    [SerializeField] private float minxMaxRoll = 30f;
+    [SerializeField] private float minMaxRoll = 30f;
     [SerializeField] private float yawPower = 4f;
+    [SerializeField] private float lerpSpeed = 2f;
 
     private DroneInputs input;
     private List<IEngine> engines = new List<IEngine>();
+    private float yaw;
+    private float finalPitch;
+    private float finalRoll;
+    private float finalYaw;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +44,15 @@ public class DroneController : BaseRigidbody
 
     protected virtual void HandleControls()
     {
+        float pitch = input.Cyclic.y * minMaxPitch;
+        float roll = -input.Cyclic.x * minMaxRoll;
+        yaw += input.Pedals * yawPower;
 
+        finalPitch = Mathf.Lerp(finalPitch, pitch, Time.deltaTime * lerpSpeed);
+        finalRoll = Mathf.Lerp(finalRoll, roll, Time.deltaTime * lerpSpeed);
+        finalYaw = Mathf.Lerp(finalYaw, yaw, Time.deltaTime * lerpSpeed);
+
+        Quaternion rotation = Quaternion.Euler(finalPitch, finalYaw, finalRoll);
+        rb.MoveRotation(rotation);
     }
 }
