bcc8d2f191ba10f872a7fb0f635aed9174ee5050
Zeshan Rasul <zeshanrasul@gmail.com>
2022-02-25 21:41:23 +0000

    Propellers rotating based on throttle, made auto hover when not moving optional

--- Quadrocopter/Assets/Scripts/Drone/DroneEngine.cs
+++ Quadrocopter/Assets/Scripts/Drone/DroneEngine.cs
@@ -7,6 +7,13 @@ public class DroneEngine : MonoBehaviour, IEngine
 {
     [Header("Engine Properties")]
     [SerializeField] private float maxPower = 4f;
+    [Header("Propeller Properties")]
+    [SerializeField] private Transform propeller;
+    [SerializeField] private float propellerRotationSpeed = 300f;
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundDistance = 0.3f;
 
     public void InitEngine()
     {
@@ -14,7 +21,7 @@ public class DroneEngine : MonoBehaviour, IEngine
 
     public void UpdateEngine(Rigidbody rb, DroneInputs inputs, bool autoHover)
     {
-        float finalDiff; 
+        float autoHoverForce; 
 
         if (autoHover)
         {
@@ -22,16 +29,28 @@ public class DroneEngine : MonoBehaviour, IEngine
             upVec.x = 0f;
             upVec.z = 0f;
             float diff = 1 - upVec.magnitude;
-            finalDiff = Physics.gravity.magnitude * diff;
+            autoHoverForce = rb.mass * Physics.gravity.magnitude + Physics.gravity.magnitude * diff;
         } 
         else
         {
-            finalDiff = 0f;
+            autoHoverForce = 0f;
         }
 
         Vector3 engineForce = Vector3.zero;
-        engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude + finalDiff) + (inputs.Throttle * maxPower)) / 4f;
+        engineForce = transform.up * (autoHoverForce + (inputs.Throttle * maxPower)) / 4f;
 
         rb.AddForce(engineForce, ForceMode.Force);
+
+        HandlePropellers(inputs.Throttle);
     }
+
+    void HandlePropellers(float throttle)
+    {
+        if (!propeller)
+            return;
+
+        if (throttle != 0)
+            propeller.Rotate(Vector3.up, throttle * propellerRotationSpeed);
+    }
+
 }
